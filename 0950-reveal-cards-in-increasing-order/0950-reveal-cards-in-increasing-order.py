b30a5835d3168906c254b4229090a1cf13288d6f
Kirsten Alafriz <80020511+kalafriz@users.noreply.github.com>
2024-04-10 15:32:50 -0400

    Time: 49 ms (35.36%), Space: 16.9 MB (9.42%) - LeetHub

---
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.py
@@ -0,0 +1,23 @@
+class Solution:
+    def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
+        
+        deck.sort()
+        result = []
+        start = 0
+        
+        # METHOD: Build the deck as it would be revealed.
+        if len(deck) <=1:
+            return deck
+        
+        for i in range(len(deck)-1, -1, -1):
+            
+            # Move bottom card to top
+            if i != len(deck)-1:
+                result = [result[-1]] + result[:-1]
+            
+            # Add next card from sorted deck to top
+            result = [deck[i]]+result
+            
+            #print(result)
+            
+        return result
